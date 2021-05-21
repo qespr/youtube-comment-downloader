88a18f53518e0f6f8b05dae1fa7c656f88ee949e
qespr <rspointer@post.cz>
2021-05-21 17:30:28 +0200

    Added some user-friendliness
    * Isolated download sequence
    * Added YouTube URL parser so we don't have to supply just IDs

--- youtube_comment_downloader/downloader.py
+++ youtube_comment_downloader/downloader.py
@@ -130,10 +130,37 @@ def search_dict(partial, search_key):
                 stack.append(value)
 
 
+def prepareDownload(ytid, outputFile, sort, limit):
+    print('Downloading Youtube comments for video:', ytid)
+    count = 0
+    with io.open(outputFile, 'w', encoding='utf8') as fp:
+        sys.stdout.write('Downloaded %d comment(s)\r' % count)
+        sys.stdout.flush()
+        start_time = time.time()
+        for comment in download_comments(ytid, sort):
+            comment_json = json.dumps(comment, ensure_ascii=False)
+            print(comment_json.decode('utf-8') if isinstance(comment_json, bytes) else comment_json, file=fp)
+            count += 1
+            sys.stdout.write('Downloaded %d comment(s)\r' % count)
+            sys.stdout.flush()
+            if limit and count >= limit:
+                break
+    print('\n[{:.2f} seconds] Done!'.format(time.time() - start_time))
+
+
+# Extracts id from (hopefully) all YouTube urls
+def extractID(source):
+    if "/youtu.be/" in source:
+        return source[source.rfind("/")+1: source.find("?")]
+    if "youtube" in source:
+        return source[source.find("?v=")+3: source.find("&")]
+    return source
+
+
 def main(argv = None):
     parser = argparse.ArgumentParser(add_help=False, description=('Download Youtube comments without using the Youtube API'))
     parser.add_argument('--help', '-h', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
-    parser.add_argument('--youtubeid', '-y', help='ID of Youtube video for which to download the comments')
+    parser.add_argument('--youtubeid', '-y', help='ID or URL of Youtube video for which to download the comments')
     parser.add_argument('--output', '-o', help='Output filename (output format is line delimited JSON)')
     parser.add_argument('--limit', '-l', type=int, help='Limit the number of comments')
     parser.add_argument('--sort', '-s', type=int, default=SORT_BY_RECENT,
@@ -155,22 +182,7 @@ def main(argv = None):
             if not os.path.exists(outdir):
                 os.makedirs(outdir)
 
-        print('Downloading Youtube comments for video:', youtube_id)
-        count = 0
-        with io.open(output, 'w', encoding='utf8') as fp:
-            sys.stdout.write('Downloaded %d comment(s)\r' % count)
-            sys.stdout.flush()
-            start_time = time.time()
-            for comment in download_comments(youtube_id, args.sort):
-                comment_json = json.dumps(comment, ensure_ascii=False)
-                print(comment_json.decode('utf-8') if isinstance(comment_json, bytes) else comment_json, file=fp)
-                count += 1
-                sys.stdout.write('Downloaded %d comment(s)\r' % count)
-                sys.stdout.flush()
-                if limit and count >= limit:
-                    break
-        print('\n[{:.2f} seconds] Done!'.format(time.time() - start_time))
-
+        prepareDownload(extractID(youtube_id), output, args.sort, limit)
     except Exception as e:
         print('Error:', str(e))
         sys.exit(1)
